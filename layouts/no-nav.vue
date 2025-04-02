753cc593d6837c2009e9e8bf5940282e7eb66a75
Dean Reymen <deenrayman@outlook.com>
2025-04-02 14:21:27 +0200

    feat: go back styling

--- layouts/no-nav.vue
+++ layouts/no-nav.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <NuxtLink to="/">Go Back</NuxtLink>
+        <div class="back-navigation">
+            <router-link to="/" class="back-link">
+                <span class="back-icon">←</span> Go back
+            </router-link>
+        </div>
         <Nuxt />
     </div>
 </template>
@@ -9,4 +13,28 @@
 export default {}
 </script>
 
-<style scoped></style>
+<style scoped>
+.back-navigation {
+    margin: 1.5rem auto;
+    padding: 0 1.5rem;
+    max-width: 800px;
+}
+
+.back-link {
+    display: inline-flex;
+    align-items: center;
+    color: #17a2b8;
+    text-decoration: none;
+    font-weight: 500;
+    transition: color 0.2s ease;
+}
+
+.back-link:hover {
+    color: #138496;
+}
+
+.back-icon {
+    margin-right: 0.5rem;
+    font-size: 1.1rem;
+}
+</style>
